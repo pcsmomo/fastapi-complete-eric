1c6a633d2fc1cb99f80877c3acf4218af42f7842
Noah Kim <dongwoo_333@hanmail.net>
2022-11-04 22:20:25 +1100

    38. Functions in Python

---
+++ 05-python-refresher/17-functions/functions.py
@@ -0,0 +1,23 @@
+"""
+Functions
+"""
+
+
+def my_function():
+    print("Inside my_function")
+
+
+my_function()
+
+
+def buy_item(cost_of_item):
+    return cost_of_item + add_tax_to_item(cost_of_item)
+
+
+def add_tax_to_item(cost_of_item):
+    current_tax_rate = .03
+    return cost_of_item * current_tax_rate
+
+
+final_cost = buy_item(50)
+print(final_cost)
