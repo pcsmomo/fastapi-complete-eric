cd8f102fa4f23fbd1795409cec7432602582b66f
Noah Kim <dongwoo_333@hanmail.net>
2022-11-11 17:21:05 +1100

    41. Imports in Python

---
+++ 05-python-refresher/19-import/homework_grades.py
@@ -0,0 +1,9 @@
+import grade_average_service as grade_service
+
+homework_assignment_grades = {
+    'homework_1': 85,
+    'homework_2': 100,
+    'homework_3': 81
+}
+
+grade_service.calculate_homework(homework_assignment_grades)
---
+++ 05-python-refresher/19-import/standardlib.py
@@ -0,0 +1,12 @@
+# Random
+import random
+import math
+
+types_of_drinks = ['Soda', 'Coffee', 'Water', 'Tea']
+print(random.choice(types_of_drinks))
+
+print(random.randint(1, 10))
+
+# Math
+square_root = math.sqrt(64)
+print(square_root)
