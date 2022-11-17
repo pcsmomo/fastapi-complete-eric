fb45f7e2e1592586ef1449b24c33a365016a4a7c
Noah Kim <dongwoo_333@hanmail.net>
2022-11-17 17:16:44 +1100

    70. FastAPI Project: Add Books to BookList

--- 08-fastapi-advanced/books2.py
+++ 08-fastapi-advanced/books2.py
@@ -19,6 +19,8 @@ BOOKS = []
 
 @app.get("/")
 async def read_all_books():
+    if len(BOOKS) < 1:
+        create_books_no_api()
     return BOOKS
 
 
@@ -26,3 +28,30 @@ async def read_all_books():
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
+
+
+def create_books_no_api():
+    book_1 = Book(id="71f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 1",
+                  author="Author 1",
+                  description="Description 1",
+                  rating=60)
+    book_2 = Book(id="21f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 2",
+                  author="Author 2",
+                  description="Description 2",
+                  rating=70)
+    book_3 = Book(id="31f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 3",
+                  author="Author 3",
+                  description="Description 3",
+                  rating=80)
+    book_4 = Book(id="41f4c2ea-1340-41f4-89f7-2852347bb0d1",
+                  title="Title 4",
+                  author="Author 4",
+                  description="Description 4",
+                  rating=90)
+    BOOKS.append(book_1)
+    BOOKS.append(book_2)
+    BOOKS.append(book_3)
+    BOOKS.append(book_4)
