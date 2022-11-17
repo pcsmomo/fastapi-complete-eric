6b180f225c98a9c71d2c38281b6de20c6b659830
Noah Kim <dongwoo_333@hanmail.net>
2022-11-18 07:56:20 +1100

    82. FastAPI Project: Headers

--- 09-http-exceptions-status-code/books2.py
+++ 09-http-exceptions-status-code/books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Request, status, Form
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
 from uuid import UUID
 from starlette.responses import JSONResponse
@@ -60,6 +60,11 @@ async def negative_number_exception_handler(request: Request,
 @app.post("/books/login")
 async def book_login(username: str = Form(), password: str = Form()):
     return {"username": username, "password": password}
+
+
+@app.get("/header")
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {"Random-Header": random_header}
 
 
 @ app.get("/")
