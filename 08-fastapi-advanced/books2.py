f629678f59e82a02ad94bd374ad9c5e288e023f2
Noah Kim <dongwoo_333@hanmail.net>
2022-11-17 18:27:22 +1100

    72. FastAPI Project: Get Request (Books 2 Enhancement)

--- 08-fastapi-advanced/books2.py
+++ 08-fastapi-advanced/books2.py
@@ -29,9 +29,17 @@ BOOKS = []
 
 
 @app.get("/")
-async def read_all_books():
+async def read_all_books(books_to_return: Optional[int] = None):
     if len(BOOKS) < 1:
         create_books_no_api()
+
+    if books_to_return and len(BOOKS) >= books_to_return > 0:
+        i = 1
+        new_books = []
+        while i <= books_to_return:
+            new_books.append(BOOKS[i])
+            i += 1
+        return new_books
     return BOOKS
 
 
