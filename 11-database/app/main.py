635d52a786028e76a752d53dcb564866b0d2f7c0
Noah Kim <dongwoo_333@hanmail.net>
2022-11-19 07:04:30 +1100

    97. FastAPI Project: Post Request (Todo Project)

--- 11-database/app/main.py
+++ 11-database/app/main.py
@@ -1,7 +1,9 @@
+from typing import Optional
 from fastapi import FastAPI, Depends, HTTPException
 import app.models as models
 from app.database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -17,6 +19,13 @@ def get_db():
         db.close()
 
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
+    complete: bool
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     # return {"Database": "Created"}
@@ -30,6 +39,23 @@ async def read_todo(todo_id: int, db: Session = Depends(get_db)):
     if todo_model is not None:
         return todo_model
     raise http_exception()
+
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
 
 
 def http_exception():
