29c09884d06fa0e036f6c4b6f90c5e37946890d7
Noah Kim <dongwoo_333@hanmail.net>
2022-11-19 20:13:58 +1100

    99. FastAPI Project: Delete Request (Todo Project)

--- 11-database/app/main.py
+++ 11-database/app/main.py
@@ -52,10 +52,7 @@ async def create_todo(todo: Todo, db: Session = Depends(get_db)):
     db.add(todo_model)
     db.commit()
 
-    return {
-        'status': 201,
-        'transaction': 'Successful'
-    }
+    return successful_response(201)
 
 
 @app.put("/{todo_id}")
@@ -73,8 +70,25 @@ async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
     db.add(todo_model)
     db.commit()
 
+    return successful_response(200)
+
+
+@app.delete("/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_exception()
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+    db.commit()
+
+    return successful_response(200)
+
+
+def successful_response(status_code: int):
     return {
-        'status': 200,
+        'status': status_code,
         'transaction': 'Successful'
     }
 
