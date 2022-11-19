2f7c1460efe0fcac3da6072728c3c9c4883c1d97
Noah Kim <dongwoo_333@hanmail.net>
2022-11-19 20:36:48 +1100

    copy the project from the previous section

---
+++ 13-authentication/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Boolean, Integer, String
+from app.database import Base
+
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
