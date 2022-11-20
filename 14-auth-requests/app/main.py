9ccac8d4599e377c844596b9fa6f7662989b6d2f
Noah Kim <dongwoo_333@hanmail.net>
2022-11-20 14:05:22 +1100

    121. FastAPI Project: Delete Todo (User ID)

--- 14-auth-requests/app/main.py
+++ 14-auth-requests/app/main.py
@@ -106,8 +106,16 @@ async def update_todo(todo_id: int,
 
 
 @app.delete("/{todo_id}")
-async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+async def delete_todo(todo_id: int,
+                      user: dict = Depends(get_current_user),
+                      db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+
+    todo_model = db.query(models.Todos)\
+        .filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == user.get("id"))\
+        .first()
 
     if todo_model is None:
         raise http_exception()
