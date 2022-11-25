5ee077bdd68e800016e55a24ca3f253d82c188fb
Noah Kim <dongwoo_333@hanmail.net>
2022-11-25 16:30:47 +1100

    141. FastAPI Project - External Routing

--- 16-routing/app/main.py
+++ 16-routing/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 import app.models as models
 from app.database import engine
 from app.routers import auth, todos
+from app.company import companyapis
 
 app = FastAPI()
 
@@ -10,3 +11,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(
+    companyapis.router,
+    prefix="/compnayapis",
+    tags=["companyapis"],
+    responses={418: {"description": "Internal Use Only"}}
+)
