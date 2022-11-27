7432110d36ecaa0f3ba6b968b7c551561fb0f30d
Noah Kim <dongwoo_333@hanmail.net>
2022-11-28 08:48:19 +1100

    161. FastAPI Full Stack - Static and CSS

--- 18-fullstack/app/main.py
+++ 18-fullstack/app/main.py
@@ -3,10 +3,13 @@ from fastapi import FastAPI, Depends
 import app.models as models
 from app.database import engine
 from app.routers import auth, todos, users
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(todos.router)
