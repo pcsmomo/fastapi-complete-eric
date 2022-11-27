4ecb110da6a340bba5b7ae21ae9bdb6dd048c1bc
Noah Kim <dongwoo_333@hanmail.net>
2022-11-27 13:34:28 +1100

    153. Alembic Enhance Application With New API

--- 17-alembic/app/routers/address.py
+++ 17-alembic/app/routers/address.py
@@ -25,9 +25,34 @@ def get_db():
 
 
 class Address(BaseModel):
-    address1 = str
-    address2 = Optional[str]
-    city = str
-    state = str
-    country = str
-    postalcode = str
+    address1: str
+    address2: Optional[str]
+    city: str
+    state: str
+    country: str
+    postalcode: str
+
+
+@router.post("/")
+async def create_address(address: Address,
+                         user: dict = Depends(get_current_user),
+                         db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    address_model = models.Address()
+    address_model.address1 = address.address1
+    address_model.address2 = address.address2
+    address_model.city = address.city
+    address_model.state = address.state
+    address_model.country = address.country
+    address_model.postalcode = address.postalcode
+
+    db.add(address_model)
+    db.flush()
+
+    user_model = db.query(models.Users).filter(models.Users.id == user.get("id")).first()
+
+    user_model.address_id = address_model.id
+
+    db.add(user_model)
+    db.commit()
