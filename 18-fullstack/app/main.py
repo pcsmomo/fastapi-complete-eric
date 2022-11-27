2b03e10a8b6f3921f29c50b1e6e27970ee22bdb2
Noah Kim <dongwoo_333@hanmail.net>
2022-11-28 08:40:12 +1100

    159. FastAPI Full Stack - API Code Cleanup

--- 18-fullstack/app/main.py
+++ 18-fullstack/app/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Depends
 import app.models as models
 from app.database import engine
 from app.routers import auth, todos, users
-from app.company import companyapis, dependencies
 
 app = FastAPI()
 
@@ -12,10 +11,10 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
-app.include_router(
-    companyapis.router,
-    prefix="/compnayapis",
-    tags=["companyapis"],
-    dependencies=[Depends(dependencies.get_token_header)],
-    responses={418: {"description": "Internal Use Only"}}
-)
+# app.include_router(
+#     companyapis.router,
+#     prefix="/compnayapis",
+#     tags=["companyapis"],
+#     dependencies=[Depends(dependencies.get_token_header)],
+#     responses={418: {"description": "Internal Use Only"}}
+# )
