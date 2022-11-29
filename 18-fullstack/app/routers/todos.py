a70a5589a24f667fe6d762c01fbfa0680104cf42
Noah Kim <dongwoo_333@hanmail.net>
2022-11-29 17:29:32 +1100

    178. FastAPI Full Stack - Delete Todo

--- 18-fullstack/app/routers/todos.py
+++ 18-fullstack/app/routers/todos.py
@@ -86,3 +86,18 @@ async def edit_todo_commit(request: Request, todo_id: int, title: str = Form(), 
     db.commit()
 
     return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id)\
+        .filter(models.Todos.owner_id == 1).first()
+
+    if todo_model is None:
+        return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
