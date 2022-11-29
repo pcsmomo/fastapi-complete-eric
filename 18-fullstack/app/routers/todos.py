186192b2ad8517b8fd643b73fdba34e0fae21706
Noah Kim <dongwoo_333@hanmail.net>
2022-11-29 17:26:17 +1100

    177. FastAPI Full Stack - Edit Todo POST

--- 18-fullstack/app/routers/todos.py
+++ 18-fullstack/app/routers/todos.py
@@ -70,3 +70,19 @@ async def add_new_todo(request: Request, todo_id: int, db: Session = Depends(get
     todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
     return templates.TemplateResponse("edit-todo.html", {"request": request, "todo": todo})
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(request: Request, todo_id: int, title: str = Form(), description: str = Form(),
+                           priority: int = Form(), db: Session = Depends(get_db)):
+
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
