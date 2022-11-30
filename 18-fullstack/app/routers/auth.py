80c74f54eb0aebe0322ff7e793d09f381957f7c5
Noah Kim <dongwoo_333@hanmail.net>
2022-12-01 08:27:52 +1100

    183. FastAPI Full Stack - Register

--- 18-fullstack/app/routers/auth.py
+++ 18-fullstack/app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status, APIRouter, Request, Response
+from fastapi import Depends, Form, HTTPException, status, APIRouter, Request, Response
 from pydantic import BaseModel
 from typing import Optional
 from passlib.context import CryptContext
@@ -136,8 +136,8 @@ async def login_for_access_token(response: Response, form_data: OAuth2PasswordRe
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         return False
-    toekn_expires = timedelta(minutes=60)
-    token = create_access_token(user.username, user.id, expires_delta=toekn_expires)
+    token_expires = timedelta(minutes=60)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
 
     response.set_cookie(key="access_token", value=token, httponly=True)
 
@@ -145,7 +145,7 @@ async def login_for_access_token(response: Response, form_data: OAuth2PasswordRe
 
 
 @router.get("/", response_class=HTMLResponse)
-async def authpage(request: Request):
+async def authentication_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
 
@@ -178,6 +178,37 @@ async def logout(request: Request):
 @router.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
+
+
+@router.post("/register", response_class=HTMLResponse)
+async def register_user(request: Request, email: str = Form(), username: str = Form(),
+                        firstname: str = Form(), lastname: str = Form(),
+                        password: str = Form(), password2: str = Form(),
+                        db: Session = Depends(get_db)):
+
+    validation1 = db.query(models.Users).filter(models.Users.username == username).first()
+
+    validation2 = db.query(models.Users).filter(models.Users.email == email).first()
+
+    if password != password2 or validation1 is not None or validation2 is not None:
+        msg = "Invalid registration request"
+        return templates.TemplateResponse("register.html", {"request": request, "msg": msg})
+
+    user_model = models.Users()
+    user_model.username = username
+    user_model.email = email
+    user_model.first_name = firstname
+    user_model.last_name = lastname
+
+    hash_password = get_password_hash(password)
+    user_model.hashed_password = hash_password
+    user_model.is_active = True
+
+    db.add(user_model)
+    db.commit()
+
+    msg = "User successfully created"
+    return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
 
 
 # Exceptions
