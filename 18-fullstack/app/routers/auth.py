b13862109d2b7bf8149193a99d89aeb061d60912
Noah Kim <dongwoo_333@hanmail.net>
2022-12-01 08:29:44 +1100

    184. FastAPI Full Stack - Code Clean Up

--- 18-fullstack/app/routers/auth.py
+++ 18-fullstack/app/routers/auth.py
@@ -28,14 +28,6 @@ ALGORITHM = keys.ALGORITHM
 templates = Jinja2Templates(directory="app/templates")
 
 
-class CreateUser(BaseModel):
-    username: str
-    email: Optional[str]
-    first_name: str
-    last_name: str
-    password: str
-
-
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 models.Base.metadata.create_all(bind=engine)  # it will issue CREATE statements for all tables
@@ -107,27 +99,10 @@ async def get_current_user(request: Request):
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if username is None or user_id is None:
-            return None
+            logout(request)
         return {"username": username, "id": user_id}
     except JWTError:
-        raise get_user_exception()
-
-
-@router.post("/create/user")
-async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
-    create_user_model = models.Users()
-    create_user_model.email = create_user.email
-    create_user_model.username = create_user.username
-    create_user_model.first_name = create_user.email
-    create_user_model.last_name = create_user.last_name
-
-    hashed_password = get_password_hash(create_user.password)
-
-    create_user_model.hashed_password = hashed_password
-    create_user_model.is_active = True
-
-    db.add(create_user_model)
-    db.commit()
+        raise HTTPException(status_code=404, detail="Not found")
 
 
 @router.post("/token")
@@ -209,22 +184,3 @@ async def register_user(request: Request, email: str = Form(), username: str = F
 
     msg = "User successfully created"
     return templates.TemplateResponse("login.html", {"request": request, "msg": msg})
-
-
-# Exceptions
-def get_user_exception():
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    return credentials_exception
-
-
-def token_exception():
-    token_exception_response = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect username or password",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    return token_exception_response
