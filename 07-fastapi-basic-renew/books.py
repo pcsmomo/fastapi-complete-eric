9b7b00a409a1e8d2a137797b8c4edf80dfcd6e3d
Noah Kim <dongwoo_333@hanmail.net>
2023-03-28 09:14:13 +1100

    54. FastAPI Project: Path Parameters

--- 07-fastapi-basic-renew/books.py
+++ 07-fastapi-basic-renew/books.py
@@ -13,5 +13,12 @@ BOOKS = [
 
 
 @app.get("/books")
-async def first_api():
+async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get("title").casefold() == book_title.casefold():
+            return book
