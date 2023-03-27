8dc188c5921dcb796c0bfc2ce57d37ce7f25821e
Noah Kim <dongwoo_333@hanmail.net>
2023-03-28 09:00:42 +1100

    52. FastAPI Project: Enhance Get Request

--- 07-fastapi-basic-renew/books.py
+++ 07-fastapi-basic-renew/books.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
 
-@app.get("/api-endpoint")
+
+@app.get("/books")
 async def first_api():
-    return {"message": "Hello Noah!"}
+    return BOOKS
