a62e042b24645e59d2cd3f444786c1cad80ddf52
Noah Kim <dongwoo_333@hanmail.net>
2023-03-29 20:51:31 +1100

    58. FastAPI Project: Post Request

--- 07-fastapi-basic-renew/books.py
+++ 07-fastapi-basic-renew/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -43,3 +43,9 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return BOOKS
