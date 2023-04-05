fd95fe7c343dcb59e1946c70858bc220f6fa8cc3
Noah Kim <dongwoo_333@hanmail.net>
2023-04-06 09:02:33 +1000

    67. FastAPI Project: Post Request before Validation

--- 08-fastapi-advanced-renew/books.py
+++ 08-fastapi-advanced-renew/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -31,3 +31,9 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/books")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
+    return book_request
