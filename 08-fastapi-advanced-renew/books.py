6436eb1bc156f6897c1d5bc38f6fb0e4047ca3b0
Noah Kim <dongwoo_333@hanmail.net>
2023-04-09 10:29:33 +1000

    70. FastAPI Project: Fields - Data Validation

--- 08-fastapi-advanced-renew/books.py
+++ 08-fastapi-advanced-renew/books.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -20,11 +22,11 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int]
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 BOOKS = [
@@ -47,5 +49,15 @@ async def create_book(book_request: BookRequest):
     # print(type(book_request))
     new_book = Book(**book_request.dict())
     # print(type(new_book))
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
     return book_request
+
+
+def find_book_id(book: Book):
+    # if len(BOOKS > 0):
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
