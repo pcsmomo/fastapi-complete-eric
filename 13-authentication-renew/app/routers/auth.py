bc6bea8556ead17f91e07dab43744ec90478de26
Noah Kim <dongwoo_333@hanmail.net>
2023-09-09 19:04:51 +1000

    103. FastAPI Project: Create First User

--- 13-authentication-renew/app/routers/auth.py
+++ 13-authentication-renew/app/routers/auth.py
@@ -1,7 +1,29 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from app.models import Users
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth/")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=create_user_request.password,
+        is_active=True,
+    )
+
+    return create_user_model
