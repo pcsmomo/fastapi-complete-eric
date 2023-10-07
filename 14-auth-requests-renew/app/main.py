4365aa617a3295bdce12828d381711a015994e51
Noah Kim <dongwoo_333@hanmail.net>
2023-10-07 19:47:50 +1100

    chore: change router file name

--- 14-auth-requests-renew/app/main.py
+++ 14-auth-requests-renew/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import app.models as models
 from app.database import engine
-from app.routers import auth, todosapp, admin
+from app.routers import auth, todos, admin
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
-app.include_router(todosapp.router)
+app.include_router(todos.router)
 app.include_router(admin.router)
