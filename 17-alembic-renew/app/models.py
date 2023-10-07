f41452675d99aac8f66ee75d5e0e92b50a73b734
Noah Kim <dongwoo_333@hanmail.net>
2023-10-08 10:42:09 +1100

    132. Alembic Installation and Setup

--- 17-alembic-renew/app/models.py
+++ 17-alembic-renew/app/models.py
@@ -13,6 +13,7 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String)
+    phone_number = Column(String)
 
 
 class Todos(Base):
