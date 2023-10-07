bf87dc69dc0c1420a017a2d56ba24b39360ef7fd
Noah Kim <dongwoo_333@hanmail.net>
2023-10-07 20:51:01 +1100

    118. FastAPI Project: Assignment Solution (Users Route)

--- 14-auth-requests-renew/app/main.py
+++ 14-auth-requests-renew/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import app.models as models
 from app.database import engine
-from app.routers import auth, todos, admin
+from app.routers import auth, todos, admin, users
 
 app = FastAPI()
 
@@ -10,3 +10,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
